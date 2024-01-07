1455a1a4ecff2fc0e6049cbefd37efb8d78f5c8d
Cyrille CHATEAU <cyrillechateau@icloud.com>
2024-01-07 16:31:52 +0100

    Initial Commit

---
+++ Chifoumi/ChifoumiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChifoumiApp.swift
+//  Chifoumi
+//
+//  Created by Cyrille Chateau on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChifoumiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
